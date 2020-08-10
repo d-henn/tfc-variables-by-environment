ad9fb5d00eebcd74f5a4b65b057769025b66f7b3
Dillon Henn <dillon.henn.f2ks@statefarm.com>
2020-08-10 11:05:09 -0700

    Update config.tf

--- yaml/config.tf
+++ yaml/config.tf
@@ -1,3 +1,3 @@
 locals {
-  variables = yamldecode(file("${path.root}/workspaces/${var.env}_variables.yaml"))
+  variables = yamldecode(file("${path.root}/workspaces/${var.tf_workspace}_variables.yaml"))
 }
